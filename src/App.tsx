c5771b32ec05acaa46edcdb638a3549d843259a0
Bruno Ribeiro <kyubruno@gmail.com>
2025-03-18 02:16:29 -0300

    Clean init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </>
+  )
+}
+
